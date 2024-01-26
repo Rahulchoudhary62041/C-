b02a1cc2993cdc70a642197bafcd2bcf5bb0ad48
Rahul Choudhary <128052435+Rahulchoudhary62041@users.noreply.github.com>
2024-01-26 20:54:37 +0530

    Add files via upload

---
+++ concatenation.cpp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+   string A = "Akash ",B="Singh",C;
+C= A + B;
+ cout << "String Concatenation :- " << C;
+  return 0;
+}
+
