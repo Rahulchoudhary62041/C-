134b6c120b11c5211996ee600c56ca3622fe2d58
Rahul Choudhary <128052435+Rahulchoudhary62041@users.noreply.github.com>
2024-02-23 17:55:05 +0530

    Add files via upload

---
+++ staticfunctions.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+using namespace std; 
+class Box 
+{ 
+	private: 
+	static int length; 
+	static int breadth; 
+	static int height; 
+	
+	public:
+	
+	static void print() 
+	{ 
+		cout << "The value of the length is: " << length << endl; 
+		cout << "The value of the breadth is: " << breadth << endl; 
+		cout << "The value of the height is: " << height << endl; 
+	}
+}; 
+int Box :: length = 10; 
+int Box :: breadth = 20; 
+int Box :: height = 30; 
+int main() 
+{
+	
+	Box b; 
+	
+	cout << "Static member function is called through Object name: \n" << endl; 
+	b.print(); 
+	
+	cout << "\nStatic member function is called through Class name: \n" << endl; 
+	Box::print(); 
+	
+	return 0; 
+}
+
