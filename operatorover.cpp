14ab0f32bccd0c51f39582e42f77c0325fdda8d3
Rahul Choudhary <128052435+Rahulchoudhary62041@users.noreply.github.com>
2024-04-11 19:49:43 +0530

    Add files via upload

---
+++ operatorover.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Prefix {
+private:
+int val;
+public:
+Prefix(){
+void Val=5;
+}
+void operator ++( ){
+Void=val+100;
+}
+void display(){
+cout<<"value":
+}};
+int main(){
+Prefix Ob;
+++ob;
+Ob.display();
+return 0;
+}
