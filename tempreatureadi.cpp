430fa7331636a1c05a1b67f38242f89dfc56cd54
Rahul Choudhary <128052435+Rahulchoudhary62041@users.noreply.github.com>
2024-06-05 20:46:16 +0530

    Add files via upload

---
+++ tempreatureadi.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int cel,fah;
+cout<<"enter the temtreature in fahraniet=";
+cin>>fah;
+cel=(fah-32)*5/9;
+cout<<"tempreature in celsius="<<cel;
+return 0;
+}
